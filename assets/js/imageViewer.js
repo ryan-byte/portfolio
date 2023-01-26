e09a8dab9479611a3ea5e4e68bd1243f54264bc2
ryan-byte <thesoulsreaper15@gmail.com>
2023-01-26 17:57:25 +0100

    fixed bugs

--- assets/js/imageViewer.js
+++ assets/js/imageViewer.js
@@ -3,7 +3,6 @@ document.querySelectorAll('img[data-toggle="modal"]').forEach((img)=>{
         $("#imageModal").modal("show");
         let modalImg = document.querySelector(".modal-img");
         let modalTitle = document.querySelector("#imageModalLabel");
-        console.log(img.parentElement);
         modalImg.src = this.getAttribute("src");
         modalTitle.innerText = this.getAttribute("alt");
     });
